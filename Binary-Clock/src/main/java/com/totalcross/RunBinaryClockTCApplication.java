731d31552dd1c89fca46c5770001a0be5e22c181
Julia Fideles <julia.hfideles@gmail.com>
2021-08-17 13:41:58 -0300

    Delete Binary Clock

--- Binary-Clock/src/main/java/com/totalcross/RunBinaryClockTCApplication.java
+++
@@ -1,9 +0,0 @@
-package com.totalcross;
-
-import totalcross.TotalCrossApplication;
-
-public class RunBinaryClockTCApplication {
-    public static void main(String [] args) {
-        TotalCrossApplication.run(BinaryClockTC.class, "/scr", "800x720","/r", "xxxxxxxxxxxxxxxxxxxxxxxx");
-    }
-}
