af12eeaef1e3bac4808aed824624b1f0439ca942
Bruno Muniz <bruno@totalcross.com>
2021-08-25 15:00:10 -0300

    Testing the application for Raspberry pi

---
+++ Binary-Clock/src/main/java/com/totalcross/RunBinaryClockTCApplication.java
@@ -0,0 +1,11 @@
+package com.totalcross;
+
+import totalcross.TotalCrossApplication;
+
+public class RunBinaryClockTCApplication {
+    public static void main(String[] args) {
+        // TotalCrossApplication.run(BinaryClockTC.class, "/scr", "android");
+        TotalCrossApplication.run(BinaryClockTC.class, "/scr", "800x480");
+
+    }
+}
